14f0178a5ae5b421520b05d31027dc9961bb22a2
rondawg369 <31311749+rondawg369@users.noreply.github.com>
2018-03-06 12:32:54 +0000

    Add files via upload

---
+++ example_WiFiConfig.h
@@ -0,0 +1,7 @@
+const char* ssid = "My_SSID";
+const char* password = "My_PASSWORD";
+const char* mqtt_server = "server.example_mqtt_server.com";
+const uint16_t mqtt_port = 123456;
+const char* clientID = "My_Client_ID";
+const char* mqtt_username = "My_MQTT_Username";
+const char* mqtt_password = "My_MQTT_Password";
